e0ec542806bcbdf46655464ab6f54d18d08c4852
Krishnaveni <veniumabala@gmail.com>
2022-12-27 14:53:21 +0000

    add

---
+++ main.js
@@ -0,0 +1,18 @@
+let date=new Date();
+console.log(date);
+let time=date.setTime(prompt("Enter the time"));
+if(time>=5 && time<10){
+    console.log("Morning");
+}
+else if(time>=10 && time<12){
+    console.log("Furnoon");
+}
+else if(time>=12 && time<16){
+    console.log("Afternoon");
+}
+else if(time>=16 && time<18){
+    console.log("Evening");
+}
+else{
+    console.log("Night");
+}
